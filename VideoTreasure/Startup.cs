83fc0c643e2f4ceadc6f3e4385306913e15adfbf
Vaishnav, Vishal <Vishal.Vaishnav@ncr.com>
2017-04-23 17:18:40 -0700

    Add project files.

---
+++ VideoTreasure/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VideoTreasure.Startup))]
+namespace VideoTreasure
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
